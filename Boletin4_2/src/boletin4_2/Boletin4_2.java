60abe599e0e0a3cec90bc4ce2eb732cd5fe28241
YasminLago <ylagorebollar@danielcastelao.org>
2015-11-04 11:26:43 +0100

    Update Boletin4_2.java

--- Boletin4_2/src/boletin4_2/Boletin4_2.java
+++ Boletin4_2/src/boletin4_2/Boletin4_2.java
@@ -2,7 +2,15 @@ package boletin4_2;
 //Yasmin
 public class Boletin4_2 {
     public static void main(String[] args) {
-        
+        int prezo,unidades,prezoTotal;
+        Artigos a=new Artigos();
+        unidades=a.unidadesArtigo();
+        prezo=a.prezoArtigo();
+        prezoTotal=a.cantidadePagar();
+        a.descontos(unidades, prezo);
+    }
+    
+}
     }
     
 }
